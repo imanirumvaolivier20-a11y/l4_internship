b70b9c8749eeabbc331f3f6fb1c25e6e4fe31f44
Imanirumva <148766123+Imanirumva@users.noreply.github.com>
2026-04-22 11:07:38 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      hello
+      <table>
+        <tr>
+          <td>id</td>
+          <td>Fname</td>
+          <td>Lname</td>
+          <td>Age</td>
+          <td>Location</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>ish</td>
+          <td>sandra</td>
+          <td>24</td>
+          <td>ngoma</td>
+        </tr>
+            <tr>
+          <td>2</td>
+          <td>mine</td>
+          <td>sandra</td>
+          <td>24</td>
+          <td>ngoma</td>
+        </tr>
+            <tr>
+          <td>3</td>
+          <td>sifa</td>
+          <td>homi</td>
+          <td>24</td>
+          <td>ngoma</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
